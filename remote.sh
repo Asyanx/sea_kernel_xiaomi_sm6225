6864acef391f0df13d8493659a997380580f7fba
Tyler Nijmeh <tylernij@gmail.com>
2019-03-13 20:21:57 -0700

    qcacld-3.0: Do not allow any wakelocks to be held
    These contribute to a great amount of idle drain.
    
    Tests: 30 minutes of playing Spotify with the screen off, unplugged.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

---
+++ remote.sh
@@ -0,0 +1,4 @@
+git remote add caf https://git.codelinaro.org/clo/la/kernel/msm-4.19
+git remote add cafbase https://github.com/asterixiverz/kernel_xiaomi_fog-msm4.19
+git remote add kribo-oc https://github.com/Siarrrrrrrr/fog-upstream-test
+git remote add mojito-patch https://github.com/backslashxx/mojito_krenol
