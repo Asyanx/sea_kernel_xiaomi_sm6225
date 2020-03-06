7894c2c8f16e94d2eb966821ae08c93d9594c643
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 03:09:00 +0330

    drivers: touchscreen: nt36525b_spi: Implement double_tap node
    Change-Id: I1ff68f35ed5b0c21f993ef339ffc3df111c60e74
    Signed-off-by: An. <noegrahachan@gmail.com>

--- drivers/input/touchscreen/nt36525b_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36525b_spi/nt36xxx.c
@@ -46,6 +46,12 @@
 #include <linux/jiffies.h>
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
+#if WAKEUP_GESTURE
+#ifdef CONFIG_TP_COMMON
+#include <linux/input/tp_common.h>
+#endif
+#endif
+
 #ifdef CHECK_TOUCH_VENDOR
 extern char *saved_command_line;
 
@@ -211,6 +217,29 @@ int nvt_ts_recovery_callback(void)
 }
 EXPORT_SYMBOL(nvt_ts_recovery_callback);
 
+#endif
+
+#ifdef CONFIG_TP_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+                               struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", ts->is_gesture_mode);
+}
+static ssize_t double_tap_store(struct kobject *kobj,
+                                struct kobj_attribute *attr, const char *buf,
+                                size_t count)
+{
+    int rc, val;
+    rc = kstrtoint(buf, 10, &val);
+    if (rc)
+    return -EINVAL;
+    lct_nvt_tp_gesture_callback(!!val);
+    return count;
+}
+static struct tp_common_ops double_tap_ops = {
+    .show = double_tap_show,
+    .store = double_tap_store
+};
 #endif
 
 /*2019.12.6 longcheer taocheng add charger mode begin*/
@@ -2234,6 +2263,14 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	ts->input_dev->event =nvt_gesture_switch;
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++)
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
+#endif
+
+#ifdef CONFIG_TP_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (ret < 0) {
+		NVT_ERR("%s: Failed to create double_tap node err=%d\n",
+                	__func__, ret);
+    }
 #endif
 
 	sprintf(ts->phys, "input/ts");
