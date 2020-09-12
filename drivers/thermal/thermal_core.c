47ac4ebb6c2fe920109ff2263519abea39eab1b7
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2020-09-12 14:35:10 +0530

    thermal: core: Workaround for xiaomi thermal mitigation
    For some reason, leaving this unconfigured making the charger voltage
    become low and it's affecting UX, set this to Evaluation by default
    to improving UX, also while on it, convert deprecated simple_strtol
    to kstrtoint
    
    Bug: Slow Charging
    Test: Charging speed normal, sconfig at 11 as defined
    
    Change-Id: Id974ceaf31da0439502372a09ff6b5f4ef2982d8
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1710,12 +1710,14 @@ static ssize_t
 thermal_sconfig_store(struct device *dev,
 				      struct device_attribute *attr, const char *buf, size_t len)
 {
-	int val = -1;
+	int ret, val = -1;
 
-	val = simple_strtol(buf, NULL, 10);
+	ret = kstrtoint(buf, 10, &val);
 
 	atomic_set(&switch_mode, val);
 
+	if (ret)
+		return ret;
 	return len;
 }
 
@@ -1752,12 +1754,14 @@ static ssize_t
 thermal_temp_state_store(struct device *dev,
 				      struct device_attribute *attr, const char *buf, size_t len)
 {
-	int val = -1;
+	int ret, val = -1;
 
-	val = simple_strtol(buf, NULL, 10);
+	ret = kstrtoint(buf, 10, &val);
 
 	atomic_set(&temp_state, val);
 
+	if (ret)
+		return ret;
 	return len;
 }
 
