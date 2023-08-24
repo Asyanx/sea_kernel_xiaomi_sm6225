87c4bc40318b76fab10397c205f6b0af6b920d53
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-24 00:18:04 +0000

    drivers: net: cnss2: Remove more logs
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -2945,8 +2945,6 @@ static int cnss_pci_runtime_suspend(struct device *dev)
 		}
 	}
 
-	cnss_pr_vdbg("Runtime suspend start\n");
-
 	driver_ops = pci_priv->driver_ops;
 	if (driver_ops && driver_ops->runtime_ops &&
 	    driver_ops->runtime_ops->runtime_suspend)
@@ -2956,8 +2954,6 @@ static int cnss_pci_runtime_suspend(struct device *dev)
 
 	if (ret)
 		pci_priv->drv_connected_last = 0;
-
-	cnss_pr_vdbg("Runtime suspend status: %d\n", ret);
 
 	return ret;
 }
@@ -2980,8 +2976,6 @@ static int cnss_pci_runtime_resume(struct device *dev)
 		return -EAGAIN;
 	}
 
-	cnss_pr_vdbg("Runtime resume start\n");
-
 	driver_ops = pci_priv->driver_ops;
 	if (driver_ops && driver_ops->runtime_ops &&
 	    driver_ops->runtime_ops->runtime_resume)
@@ -2992,15 +2986,11 @@ static int cnss_pci_runtime_resume(struct device *dev)
 	if (!ret)
 		pci_priv->drv_connected_last = 0;
 
-	cnss_pr_vdbg("Runtime resume status: %d\n", ret);
-
 	return ret;
 }
 
 static int cnss_pci_runtime_idle(struct device *dev)
 {
-	cnss_pr_vdbg("Runtime idle\n");
-
 	pm_request_autosuspend(dev);
 
 	return -EBUSY;
