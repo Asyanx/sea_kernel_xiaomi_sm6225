801450d7bf86f4af0e170885e10c10e423beca71
Asyanx <wowsanja2@gmail.com>
2024-04-04 22:13:38 +0000

    sched/cass: `adapt 4.19` Perform runqueue selection for RT tasks too

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -223,13 +223,12 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 }
 
 static int cass_select_task_rq(struct task_struct *p, int prev_cpu,
-				    int sd_flag, int wake_flags, 
-				    int sibling_count_hint, bool rt)
+				    int wake_flags, bool rt)
 {
 	bool sync;
 
 	/* Don't balance on exec since we don't know what @p will look like */
-	if (sd_flag & SD_BALANCE_EXEC)
+	if (wake_flags & SD_BALANCE_EXEC)
 		return prev_cpu;
 
 	/*
@@ -241,7 +240,7 @@ static int cass_select_task_rq(struct task_struct *p, int prev_cpu,
 		return cpumask_first(p->cpus_ptr);
 
 	/* cass_best_cpu() needs the CFS task's utilization, so sync it up */
-	if (!rt && !(sd_flag & SD_BALANCE_FORK))
+	if (!rt && !(wake_flags & SD_BALANCE_FORK))
 		sync_entity_load_avg(&p->se);
 
 	sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
@@ -249,12 +248,13 @@ static int cass_select_task_rq(struct task_struct *p, int prev_cpu,
 }
 
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
-				    int wake_flags)
+				    int sd_flag, int wake_flags, int sibling_count_hint)
 {
 	return cass_select_task_rq(p, prev_cpu, wake_flags, false);
 }
 
-int cass_select_task_rq_rt(struct task_struct *p, int prev_cpu, int wake_flags)
+int cass_select_task_rq_rt(struct task_struct *p, int prev_cpu,
+ 			   int sd_flag, int wake_flags, int sibling_count_hint)
 {
 	return cass_select_task_rq(p, prev_cpu, wake_flags, true);
 }
