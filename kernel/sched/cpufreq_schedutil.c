f257e50d76a32ad7c7825f42e4aee36b0a735aae
Asyanx <wowsanja2@gmail.com>
2024-10-21 04:48:43 +0000

    cpufreq: schedutil: exponential freq selection
    This reverts commit b1516544f5c36cf337280ab5bffd11b83d2601f4.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -24,6 +24,7 @@ struct sugov_tunables {
 	struct gov_attr_set	attr_set;
 	unsigned int		up_rate_limit_us;
 	unsigned int		down_rate_limit_us;
+	bool exp_util;
 };
 
 struct sugov_policy {
@@ -221,7 +222,10 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
         unsigned int freq;
         unsigned int idx, l_freq, h_freq;
 
-	freq = map_util_freq(util, freq, max);
+	if (sg_policy->tunables->exp_util)
+		freq = (freq + (freq >> 2)) * int_sqrt(util * 100 / max) / 10;
+	else
+		freq = map_util_freq(util, freq, max);
 
 	if (arch_scale_freq_invariant())
 		freq = policy->cpuinfo.max_freq;
@@ -759,12 +763,32 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	return count;
 }
 
+static ssize_t exp_util_show(struct gov_attr_set *attr_set, char *buf)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", tunables->exp_util);
+}
+
+static ssize_t exp_util_store(struct gov_attr_set *attr_set, const char *buf,
+				   size_t count)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	if (kstrtobool(buf, &tunables->exp_util))
+		return -EINVAL;
+
+	return count;
+}
+
 static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
 static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
+static struct governor_attr exp_util = __ATTR_RW(exp_util);
 
 static struct attribute *sugov_attributes[] = {
 	&up_rate_limit_us.attr,
 	&down_rate_limit_us.attr,
+	&exp_util.attr,
 	NULL
 };
 
@@ -883,6 +907,7 @@ static void sugov_tunables_save(struct cpufreq_policy *policy,
 			per_cpu(cached_tunables, cpu) = cached;
 	}
 
+	cached->exp_util = tunables->exp_util;
 	cached->up_rate_limit_us = tunables->up_rate_limit_us;
 	cached->down_rate_limit_us = tunables->down_rate_limit_us;
 }
@@ -902,6 +927,7 @@ static void sugov_tunables_restore(struct cpufreq_policy *policy)
 	if (!cached)
 		return;
 
+	tunables->exp_util = cached->exp_util;
 	tunables->up_rate_limit_us = cached->up_rate_limit_us;
 	tunables->down_rate_limit_us = cached->down_rate_limit_us;
 }
@@ -950,6 +976,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
         tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
+        tunables->exp_util = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
