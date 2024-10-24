53265c0119b35fd413aebb816d663c328af58578
alternoegraha <noegrahachan@gmail.com>
2024-10-24 09:45:55 +0000

    Drop old implementation of SusFS
    Revert "fs: prevent Zygisk/KernelSU detection through common methods"
    
    This reverts commit 49f24c34a877e08380438b05a1571e0b4d5e83d8.
    
    + fix Conflicts

--- fs/namei.c
+++ fs/namei.c
@@ -40,10 +40,6 @@
 #include <linux/init_task.h>
 #include <linux/uaccess.h>
 #include <linux/build_bug.h>
-#ifdef CONFIG_SUS_FS
-#include <linux/suspicious.h>
-#endif
-
 #include "internal.h"
 #include "mount.h"
 
@@ -3706,12 +3702,6 @@ struct file *do_filp_open(int dfd, struct filename *pathname,
 	int flags = op->lookup_flags;
 	struct file *filp;
 
-#ifdef CONFIG_SUS_FS
-	if (suspicious_path(pathname)) {
-		return ERR_PTR(-ENOENT);
-	}
-#endif
-	
 	set_nameidata(&nd, dfd, pathname);
 	filp = path_openat(&nd, op, flags | LOOKUP_RCU);
 	if (unlikely(filp == ERR_PTR(-ECHILD)))
@@ -3903,18 +3893,6 @@ long do_mknodat(int dfd, const char __user *filename, umode_t mode,
 	struct dentry *dentry;
 	struct path path;
 	int error;
-#ifdef CONFIG_SUS_FS
-	struct filename* fname;
-	int status;
-	
-	fname = getname_safe(filename);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-#endif
 	unsigned int lookup_flags = 0;
 
 	error = may_mknod(mode);
@@ -4002,18 +3980,6 @@ long do_mkdirat(int dfd, const char __user *pathname, umode_t mode)
 	struct path path;
 	int error;
 	unsigned int lookup_flags = LOOKUP_DIRECTORY;
-#ifdef CONFIG_SUS_FS
-	struct filename* fname;
-	int status;
-
-	fname = getname_safe(pathname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-#endif
 
 retry:
 	dentry = user_path_create(dfd, pathname, &path, lookup_flags);
@@ -4098,12 +4064,6 @@ long do_rmdir(int dfd, const char __user *pathname)
 	int type;
 	unsigned int lookup_flags = 0;
 
-#ifdef CONFIG_SUS_FS
-	if (suspicious_path(name)) {
-		return -ENOENT;
-	}
-#endif
-	
 retry:
 	name = filename_parentat(dfd, getname(pathname), lookup_flags,
 				&path, &last, &type);
@@ -4351,27 +4311,7 @@ long do_symlinkat(const char __user *oldname, int newdfd,
 	struct dentry *dentry;
 	struct path path;
 	unsigned int lookup_flags = 0;
-#ifdef CONFIG_SUS_FS
-	struct filename* fname;
-	int status;
 
-	fname = getname_safe(oldname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-
-	fname = getname_safe(newname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-#endif
-	
 	from = getname(oldname);
 	if (IS_ERR(from))
 		return PTR_ERR(from);
@@ -4508,27 +4448,7 @@ int do_linkat(int olddfd, const char __user *oldname, int newdfd,
 	struct inode *delegated_inode = NULL;
 	int how = 0;
 	int error;
-#ifdef CONFIG_SUS_FS
-	struct filename* fname;
-	int status;
 
-	fname = getname_safe(oldname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-
-	fname = getname_safe(newname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-#endif
-	
 	if ((flags & ~(AT_SYMLINK_FOLLOW | AT_EMPTY_PATH)) != 0)
 		return -EINVAL;
 	/*
@@ -4794,27 +4714,7 @@ static int do_renameat2(int olddfd, const char __user *oldname, int newdfd,
 	unsigned int lookup_flags = 0, target_flags = LOOKUP_RENAME_TARGET;
 	bool should_retry = false;
 	int error;
-#ifdef CONFIG_SUS_FS
-	struct filename* fname;
-	int status;
 
-	fname = getname_safe(oldname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-
-	fname = getname_safe(newname);
-	status = suspicious_path(fname);
-	putname_safe(fname);
-
-	if (status) {
-		return -ENOENT;
-	}
-#endif
-	
 	if (flags & ~(RENAME_NOREPLACE | RENAME_EXCHANGE | RENAME_WHITEOUT))
 		return -EINVAL;
 
@@ -4836,26 +4736,12 @@ retry:
 		goto exit;
 	}
 
-#ifdef CONFIG_SUS_FS
-	if (suspicious_path(from)) {
-		error = -ENOENT;
-		goto exit;
-	}
-#endif
-	
 	to = filename_parentat(newdfd, getname(newname), lookup_flags,
 				&new_path, &new_last, &new_type);
 	if (IS_ERR(to)) {
 		error = PTR_ERR(to);
 		goto exit1;
 	}
-
-#ifdef CONFIG_SUS_FS	
-	if (suspicious_path(to)) {
-		error = -ENOENT;
-		goto exit;
-	}
-#endif
 
 	error = -EXDEV;
 	if (old_path.mnt != new_path.mnt)
