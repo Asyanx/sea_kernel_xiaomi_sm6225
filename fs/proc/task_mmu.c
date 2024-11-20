a1a191877f0aa142cf06dd9b6f004e04bac47e4f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-21 03:08:23 +0800

    /proc/pid/maps: fake all lineage
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -389,7 +389,13 @@ bypass_orig_flow:
         struct dentry *dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage")) { return; }
+            	if (strstr(path, "lineage")) { 
+	  	start = vma->vm_start;
+		end = vma->vm_end;
+		show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
+            	name = "/dev/ashmem (deleted)";
+		goto done;
+            	 	}
             	}
 	}
 
