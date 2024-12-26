b0ad01b2c1d1840e53efbdd3dd0bd5ca8fa26b0d
simonpunk <simonpunk2016@gmail.com>
2024-12-26 21:45:44 +0700

    fs: susfs: Make SUSFS compatible with Magic Mount KernelSU
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,14 +4,14 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
-extern int susfs_spoof_proc_cmdline(struct seq_file *m);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
-	if (!susfs_spoof_proc_cmdline(m)) {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
 		seq_putc(m, '\n');
 		return 0;
 	}
