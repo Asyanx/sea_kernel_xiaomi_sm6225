bb6cc23058ff8cb1db437affd6dbdf335abb6ae5
Asyanx <wowsanja2@gmail.com>
2025-01-01 02:17:54 +0000

    Revert "cpuset: Don't set cpus allowed for perf critical task"
    This reverts commit 5f3673234e807275eaf1824cf64ec318a3c79b9a.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2497,9 +2497,8 @@ void cpuset_cpus_allowed(struct task_struct *tsk, struct cpumask *pmask)
 void cpuset_cpus_allowed_fallback(struct task_struct *tsk)
 {
 	rcu_read_lock();
-	if(!(tsk->flags & PF_PERF_CRITICAL))
-		do_set_cpus_allowed(tsk, is_in_v2_mode() ?
-			task_cs(tsk)->cpus_allowed : cpu_possible_mask);
+	do_set_cpus_allowed(tsk, is_in_v2_mode() ?
+		task_cs(tsk)->cpus_allowed : cpu_possible_mask);
 	rcu_read_unlock();
 
 	/*
