025977fc07cde68d51efb24c4f51ec62bd090183
simonpunk <simonpunk2016@gmail.com>
2025-01-08 18:56:54 +0700

    susfs: Better handling of umounting for default ksu umount scheme by checking if the mnt_devname is KSU instead of dev type overlay, this may fix some wired issue for phones that mounts overlay on some system filesystem by system process but not KSU
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- KernelSU/kernel/core_hook.c
+++ KernelSU/kernel/core_hook.c
@@ -759,11 +759,15 @@ static bool should_umount(struct path *path)
 		return false;
 	}
 
+#ifdef CONFIG_KSU_SUSFS
+	return susfs_is_mnt_devname_ksu(path);
+#else
 	if (path->mnt && path->mnt->mnt_sb && path->mnt->mnt_sb->s_type) {
 		const char *fstype = path->mnt->mnt_sb->s_type->name;
 		return strcmp(fstype, "overlay") == 0;
 	}
 	return false;
+#endif
 }
 
 static void ksu_umount_mnt(struct path *path, int flags)
@@ -807,8 +811,8 @@ void susfs_try_umount_all(uid_t uid) {
 	ksu_try_umount("/vendor", true, 0);
 	ksu_try_umount("/product", true, 0);
 	ksu_try_umount("/odm", true, 0);
-	ksu_try_umount("/data/adb/modules", false, MNT_DETACH);
-	ksu_try_umount("/debug_ramdisk", false, MNT_DETACH);
+	ksu_try_umount("/data/adb/modules", true, MNT_DETACH);
+	ksu_try_umount("/debug_ramdisk", true, MNT_DETACH);
 }
 #endif
 
