0044daa4631b7d9f24cd11bee359cb6c91295507
asyanx <wowsanja2@gmail.com>
2026-09-25 00:09:44 +0700

    Revert "KernelSU: update to backslashxx/KernelSU/12049-susfs155"
    This reverts commit 9cb9484c2f6127b860642d4d4edc052a8f9f9c81.

--- KernelSU/kernel/core_hook.h
+++ KernelSU/kernel/core_hook.h
@@ -4,5 +4,6 @@
 #include <linux/init.h>
 
 void __init ksu_core_init(void);
+void ksu_core_exit(void);
 
 #endif
--- KernelSU/kernel/ksu.c
+++ KernelSU/kernel/ksu.c
@@ -36,6 +36,11 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 					    flags);
 }
 
+extern void ksu_sucompat_init();
+extern void ksu_sucompat_exit();
+extern void ksu_ksud_init();
+extern void ksu_ksud_exit();
+
 int __init ksu_kernelsu_init(void)
 {
 #ifdef CONFIG_KSU_DEBUG
@@ -60,6 +65,16 @@ int __init ksu_kernelsu_init(void)
 
 	ksu_throne_tracker_init();
 
+#ifdef CONFIG_KPROBES
+	ksu_sucompat_init();
+	ksu_ksud_init();
+#endif
+
+#ifdef MODULE
+#ifndef CONFIG_KSU_DEBUG
+	kobject_del(&THIS_MODULE->mkobj.kobj);
+#endif
+#endif
 	return 0;
 }
 
@@ -71,6 +86,12 @@ void ksu_kernelsu_exit(void)
 
 	destroy_workqueue(ksu_workqueue);
 
+#ifdef CONFIG_KPROBES
+	ksu_ksud_exit();
+	ksu_sucompat_exit();
+#endif
+
+	ksu_core_exit();
 }
 
 module_init(ksu_kernelsu_init);
