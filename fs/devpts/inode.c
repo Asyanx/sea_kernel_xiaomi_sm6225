cde8821584b9e3e71647b8311ebadeeb57d278a5
asyanx <wowsanja2@gmail.com>
2026-09-25 00:09:51 +0700

    Revert "KernelSU: devpts hook"
    This reverts commit bf00e141f9548b4a4fd168dba9a089218a0c70f0.

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -599,7 +599,8 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
@@ -611,9 +612,6 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_devpts(dentry->d_inode);
-#endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
