86c6df09ec47174aa36a4f1619c823fe9723836b
asyanx <wowsanja2@gmail.com>
2026-09-25 00:09:51 +0700

    Revert "fs: susfs: Small improvement on SPOOF_UNAME"
    This reverts commit d7a19207f6254ce768f5dff5fee20a76ad02b701.

--- fs/susfs.c
+++ fs/susfs.c
@@ -659,11 +659,16 @@ int susfs_set_uname(struct st_susfs_uname* __user user_info) {
 	return 0;
 }
 
-void susfs_spoof_uname(struct new_utsname* tmp) {
+int susfs_spoof_uname(struct new_utsname* tmp) {
 	if (unlikely(my_uname.release[0] == '\0' || spin_is_locked(&susfs_uname_spin_lock)))
-		return;
+		return 1;
+	strncpy(tmp->sysname, utsname()->sysname, __NEW_UTS_LEN);
+	strncpy(tmp->nodename, utsname()->nodename, __NEW_UTS_LEN);
 	strncpy(tmp->release, my_uname.release, __NEW_UTS_LEN);
 	strncpy(tmp->version, my_uname.version, __NEW_UTS_LEN);
+	strncpy(tmp->machine, utsname()->machine, __NEW_UTS_LEN);
+	strncpy(tmp->domainname, utsname()->domainname, __NEW_UTS_LEN);
+	return 0;
 }
 #endif // #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 
